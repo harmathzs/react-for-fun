5bbf94ed59fb89239961d9eddbfd5c83f22e32be
Zsolt Harmath <harmathzs@gmail.com>
2026-05-06 22:12:08 +0200

    Refactor App component to implement lead form structure and navigation

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,34 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App(props) {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>Hello</p>
+      <nav>
+        <a>Interest in products</a>
+        <a>Future menuitem</a>
+        <a>Future menuitem</a>
+      </nav>
+      <section>
+        <article>
+          <form>
+            TODO
+            {/* If Interest in products is checked, then here comes the Web-to-Lead form generated from Salesforce and styled. */}
+          </form>
+        </article>
+      </section>
     </>
   )
 }
-
-export default App
